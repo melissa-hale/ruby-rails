5ef8882ebc3a36af0dd82b2d0d7cd1865629a8e5
Melissa Hale <melissakayh@gmail.com>
2024-04-10 13:20:53 +0200

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,10 +3,10 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.hosts << ENV["RAILWAY_PUBLIC_DOMAIN"]
+  
   # Code is not reloaded between requests.
   config.cache_classes = true
-
-  config.hosts << ENV["RAILWAY_PUBLIC_DOMAIN"]
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
